703f4593106ce304e8c5a50e4b8a93ca2949adef
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2024-01-15 18:42:59 +0900

    Update TikTok username validation

--- src/__tests__/valibot.test.ts
+++ src/__tests__/valibot.test.ts
@@ -21,22 +21,33 @@ describe("x", () => {
 describe("tiktok", () => {
   test("TikTok handle validation", () => {
     const validate = tiktok();
-    expect(validate._parse("Valid.Name123").output).toBe("Valid.Name123");
-    expect(validate._parse("Valid_Name.123").output).toBe("Valid_Name.123");
 
-    // Usernames ending in a period are invalid
+    // Valid usernames
+    expect(validate._parse("Va").output).toBe("Va");
+    expect(validate._parse("Valid.Name123").output).toBe("Valid.Name123");
+    expect(validate._parse("Username_With_24_Charact").output).toBe(
+      "Username_With_24_Charact"
+    );
+
+    // Invalid due to ending with a period
     expect(validate._parse("InvalidName.").issues).toBeTruthy();
 
-    // Usernames with too many characters are invalid
-    expect(validate._parse("TooLongUsername12345").issues).toBeTruthy();
+    // Invalid due to being too short
+    expect(validate._parse("I").issues).toBeTruthy();
 
-    // Usernames containing invalid characters are invalid
+    // Invalid due to exceeding 24 characters
+    expect(
+      validate._parse("TooLongUsernameExceeding24Chars").issues
+    ).toBeTruthy();
+
+    // Invalid due to containing special characters
     expect(validate._parse("Invalid*Name").issues).toBeTruthy();
   });
 
   test("should return custom error message", () => {
     const customError = "Custom error message";
     const validate = tiktok(customError);
+    // Testing with a custom error message
     expect(validate._parse("Invalid*Name").issues?.[0].message).toBe(
       customError
     );
--- src/valibot/tiktok.ts
+++ src/valibot/tiktok.ts
@@ -11,13 +11,13 @@ export type XValidation<TInput extends string> = BaseValidation<TInput> & {
 };
 
 export function tiktok<TInput extends string>(
-  message: ErrorMessage = "Usernames can only contain letters, numbers, underscores, and periods. However, periods can't be added to the end of a username."
+  message: ErrorMessage = " Usernames can only contain letters, numbers, underscores, and periods. However, periods can't be added to the end of a username."
 ): XValidation<TInput> {
   return {
     type: "tiktok",
     async: false,
     message,
-    requirement: /^[a-zA-Z0-9_.]{1,15}(?<!\.)$/,
+    requirement: /^[a-zA-Z0-9_.]{2,24}(?<!\.)$/,
     _parse(input) {
       return !this.requirement.test(input)
         ? actionIssue(this.type, this.message, input, this.requirement)
