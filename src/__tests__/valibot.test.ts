b378ac8f99138ad03b16db8717125474efa98066
yutakobaayshidev <91340399+yutakobayashidev@users.noreply.github.com>
2024-01-15 18:32:27 +0900

    Add TikTok handle validation to valibot

--- src/__tests__/valibot.test.ts
+++ src/__tests__/valibot.test.ts
@@ -1,4 +1,5 @@
 import { x } from "../valibot/x";
+import { tiktok } from "../valibot/tiktok";
 
 describe("x", () => {
   test("X (formerly Twitter) handle validation", () => {
@@ -14,5 +15,30 @@ describe("x", () => {
     const error = "Custom error message";
     const validate = x(error);
     expect(validate._parse("Invalid*Name").issues?.[0].message).toBe(error);
+  });
+});
+
+describe("tiktok", () => {
+  test("TikTok handle validation", () => {
+    const validate = tiktok();
+    expect(validate._parse("Valid.Name123").output).toBe("Valid.Name123");
+    expect(validate._parse("Valid_Name.123").output).toBe("Valid_Name.123");
+
+    // Usernames ending in a period are invalid
+    expect(validate._parse("InvalidName.").issues).toBeTruthy();
+
+    // Usernames with too many characters are invalid
+    expect(validate._parse("TooLongUsername12345").issues).toBeTruthy();
+
+    // Usernames containing invalid characters are invalid
+    expect(validate._parse("Invalid*Name").issues).toBeTruthy();
+  });
+
+  test("should return custom error message", () => {
+    const customError = "Custom error message";
+    const validate = tiktok(customError);
+    expect(validate._parse("Invalid*Name").issues?.[0].message).toBe(
+      customError
+    );
   });
 });
---
+++ src/valibot/tiktok.ts
@@ -0,0 +1,27 @@
+import {
+  type BaseValidation,
+  ErrorMessage,
+  actionIssue,
+  actionOutput,
+} from "valibot";
+
+export type XValidation<TInput extends string> = BaseValidation<TInput> & {
+  type: "tiktok";
+  requirement: RegExp;
+};
+
+export function tiktok<TInput extends string>(
+  message: ErrorMessage = "Usernames can only contain letters, numbers, underscores, and periods. However, periods can't be added to the end of a username."
+): XValidation<TInput> {
+  return {
+    type: "tiktok",
+    async: false,
+    message,
+    requirement: /^[a-zA-Z0-9_.]{1,15}(?<!\.)$/,
+    _parse(input) {
+      return !this.requirement.test(input)
+        ? actionIssue(this.type, this.message, input, this.requirement)
+        : actionOutput(input);
+    },
+  };
+}
